225cd3615fd695b3c1e2a8083cc0b10de296ad1f
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-10-21 14:08:52 +0300

    Update 𝖢𝖧𝖠𝖢𝖪𝖤𝖱.py

--- 𝖢𝖧𝖠𝖢𝖪𝖤𝖱.py
+++ 𝖢𝖧𝖠𝖢𝖪𝖤𝖱.py
@@ -1,4 +1,4 @@
-import random
+import socket, random, time
 print('''
 
 ░█████╗░██╗░░██╗░█████╗░░█████╗░██╗░░██╗███████╗
@@ -22,55 +22,29 @@ print('''
 |=================================================|
 |      [ + ] Telegram : https://t.me/kavani2knn10 |                   
 |=================================================| 
+
+==========================================
+- four letters [1]\n  
+- five letters [2]\n
+==========================================
 ''')
+choose = input("please choose option : ") 
+if choose == '1':
+         while True:
+          A = "abcdefghijkImnopqrstuvwxyz"
+          a = "123456789"
+          all=A + a 
+          length1 = 4    
+          user ="".join(random.sample(all,length1))
+          print(""+user)
+          time.sleep(0.7)
 
-
-
-
-A = "abcdefghijkImnopqrstuvwxyz"
-a = "123456789"
-all=A + a 
-length = 5
-length1 = 4
-user ="".join(random.sample(all,length))
-user1 ="".join(random.sample(all,length1))
-user2 ="".join(random.sample(all,length))
-user3 ="".join(random.sample(all,length1))
-user4 ="".join(random.sample(all,length)) 
-user5 ="".join(random.sample(all,length1))
-user6 ="".join(random.sample(all,length))
-user7 ="".join(random.sample(all,length1))
-user8 ="".join(random.sample(all,length))
-user9 ="".join(random.sample(all,length1))
-user10 ="".join(random.sample(all,length))
-user11 ="".join(random.sample(all,length1))
-user12 ="".join(random.sample(all,length))
-user13 ="".join(random.sample(all,length1))
-user14 ="".join(random.sample(all,length))
-user15 ="".join(random.sample(all,length1))
-user16 ="".join(random.sample(all,length))
-user17 ="".join(random.sample(all,length1))
-user18 ="".join(random.sample(all,length))
-user19 ="".join(random.sample(all,length1))
-user20 ="".join(random.sample(all,length))
-print(""+user)
-print(""+user1)
-print(""+user2)
-print(""+user3)
-print(""+user4)
-print(""+user5)
-print(""+user6)
-print(""+user7)
-print(""+user8)
-print(""+user9)
-print(""+user10)
-print(""+user11)
-print(""+user12)
-print(""+user13)
-print(""+user14)
-print(""+user15)
-print(""+user16)
-print(""+user17)
-print(""+user18)
-print(""+user19)
-print(""+user20)
+if choose == '2':
+     while True:
+          A = "abcdefghijkImnopqrstuvwxyz"
+          a = "123456789"
+          all=A + a 
+          length1 = 5    
+          user ="".join(random.sample(all,length1))
+          print(""+user)
+          time.sleep(0.7)
